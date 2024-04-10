6db50a50452093ac2db5dba10620f45d5fa72ad8
abrooks90 <andrew.brooks.aob@gmail.com>
2024-04-10 16:28:44 -0400

    Const correctness for Tuple

--- include/tuple.h
+++ include/tuple.h
@@ -18,10 +18,10 @@ public:
 
 	// It's unclear why the book has me measuring W as part of Magnitude but it's there so I'm putting it here.
 	// Magnitude is only measured for a Vector and W will always be 0.
-	float Magnitude();
-	Tuple Normalize();
-	float DotProduct(Tuple t);
-	Tuple CrossProduct(Tuple t);
+	float Magnitude() const;
+	Tuple Normalize() const;
+	float DotProduct(Tuple t) const;
+	Tuple CrossProduct(Tuple t) const;
 
 private:
 	float x_, y_, z_, w_;
@@ -33,7 +33,7 @@ Tuple Point(float x, float y, float z);
 Tuple operator +(Tuple lhs, Tuple rhs);
 Tuple operator -(Tuple lhs, Tuple rhs);
 Tuple operator -(Tuple t);
-Tuple operator *(float lhs, Tuple rhs);
+Tuple operator *(Tuple rhs, float lhs);
 Tuple operator /(Tuple lhs, float rhs);
 bool operator ==(Tuple lhs, Tuple rhs);
 
